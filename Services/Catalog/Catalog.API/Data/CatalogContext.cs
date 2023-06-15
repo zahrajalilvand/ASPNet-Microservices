8d1f8c5dcf058775a0a169b62fcd2a5849397179
zahra.jalilvand <zahra.jalilvand@gmail.com>
2023-06-15 12:59:52 +0330

    Commit Initial Context

---
+++ Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Products> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings: ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings: DatabaseName"));
+            Products = database.GetCollection<Products>(configuration.GetValue<string>("DatabaseSettings: CollectioName"));
+            CatalogContextSeed.DataSeed(Products);
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,63 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void DataSeed(IMongoCollection<Products> ProductCollection) 
+        { 
+            bool existData = ProductCollection.Find(p => true).Any();
+            if (!existData) {
+                ProductCollection.InsertManyAsync(GetProductData());
+            }
+        }
+
+        private static IEnumerable<Products> GetProductData()
+        {
+            return new List<Products>()
+            {
+                new Products()
+                {
+                    Id = "602d2149e773fa2a3990b47f5",
+                    Name = "IPhone x",
+                    Summary = "Description",
+                    Description = "Description",
+                    imageFile = "Product-1.png",
+                    Price = 100.00M,
+                    Category = "Category"
+                },
+                new Products()
+                {
+                    Id = "602d2149e773fa2a3990b47f6",
+                    Name = "IPhone 10",
+                    Summary = "Description",
+                    Description = "Description",
+                    imageFile = "Product-1.png",
+                    Price = 150.00M,
+                    Category = "Category"
+                },
+                new Products()
+                {
+                    Id = "602d2149e773fa2a3990b47f7",
+                    Name = "IPhone 11",
+                    Summary = "Description",
+                    Description = "Description",
+                    imageFile = "Product-1.png",
+                    Price = 400.00M,
+                    Category = "Category"
+                },
+                new Products()
+                {
+                    Id = "602d2149e773fa2a3990b47f8",
+                    Name = "IPhone 12",
+                    Summary = "Description",
+                    Description = "Description",
+                    imageFile = "Product-1.png",
+                    Price = 500.00M,
+                    Category = "Category"
+                }
+            };
+        }
+    }
+}
