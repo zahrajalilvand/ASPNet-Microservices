89d211d3149de234561a7bcae890384a06fb4952
zahra.jalilvand <zahra.jalilvand@gmail.com>
2023-06-19 07:23:29 +0330

    commit cotroller to project

---
+++ Services/Catalog/Catalog.API/Controller/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        #region Constructor
+        private readonly ICatalogContext _catalogContext;
+        private readonly IProductRepository _productRepository;
+
+        public CatalogController()
+        {
+            
+        }
+        #endregion
+    }
+}
